4aa929b98bc4790f138ba5ea235c6cbd82559d73
Slater <armando.jaimes@pucp.edu.pe>
2024-08-31 11:57:12 -0500

    Configuration Loader

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,22 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { ServeStaticModule } from '@nestjs/serve-static';
-import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot(),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB),
 
     PokemonModule,
 
@@ -22,4 +26,9 @@ import { SeedModule } from './seed/seed.module';
 
   ],
 })
-export class AppModule { }
+export class AppModule {
+
+  constructor() {
+    console.log(process.env)
+  }
+}
