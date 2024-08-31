d6e4db9645ca3d5914ffaa0b2ff3c28f89c013ed
Slater <armando.jaimes@pucp.edu.pe>
2024-08-31 12:27:02 -0500

    ConfigurationService

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,14 @@ import { ConfigModule } from '@nestjs/config';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/app.config';
 
 @Module({
   imports: [
 
-    ConfigModule.forRoot(),
+    ConfigModule.forRoot({
+      load: [EnvConfiguration]
+    }),
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
@@ -26,9 +29,4 @@ import { SeedModule } from './seed/seed.module';
 
   ],
 })
-export class AppModule {
-
-  constructor() {
-    console.log(process.env)
-  }
-}
+export class AppModule { }
